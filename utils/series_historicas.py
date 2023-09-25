78a20914ecf247b96c3f48de62dd5dd715c7a21b
Lucas Rodrigues <lucasrodrigues@sobral.gov.ce.br>
2023-09-25 18:01:13 -0300

    feat: pib e ipca

--- utils/series_historicas.py
+++ utils/series_historicas.py
@@ -1,15 +1,49 @@
 from bcb import sgs
 from time import  sleep
+import ipeadatapy
+import numpy as np
+
+def __get_code_pib():
+    return 'PAN4_PIBPMG4'
+
+def __get_code_ipca():
+    return 'PRECOS12_IPCAG12'
+
+def porcentagem(serie):
+    serie = [[valor / 100 for valor in lista] for lista in serie]
+    return serie
+
+def sgs_get(codigo):
+    continua = True
+    while continua:
+        try: 
+            serie = sgs.get(codigo)
+            continua = False
+        except:
+            sleep(1)
+
+    return serie
+
+
 
 def pegar_serie_ipca():
-    try:
-        serie_ipca = sgs.get(433)
-    except:
-        sleep(1)
-        serie_ipca = sgs.get(433)
-
-    serie_ipca = serie_ipca.values.tolist()
-
-    serie_ipca = [[valor / 100 for valor in lista] for lista in serie_ipca]
-
+    code = __get_code_ipca()
+    serie_ipca = ipeadatapy.timeseries(code)[f'VALUE ((% a.m.))']
+    serie_ipca = serie_ipca.values
+    serie_ipca = serie_ipca / 100
+    
     return serie_ipca
+
+
+def pegar_serie_pib():
+    code = __get_code_pib()
+    pib_var_trim = ipeadatapy.timeseries(code)[f'VALUE ((% a.a.))']
+    pib_var_trim = pib_var_trim.values
+    pib_var_trim = pib_var_trim / 100
+
+    return pib_var_trim
+
+
+
+
+pegar_serie_pib()
