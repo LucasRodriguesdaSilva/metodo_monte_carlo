872b37598679ed50186e27e7212b05704acb1f86
Lucas Rodrigues <lucasrodrigues@sobral.gov.ce.br>
2023-10-03 08:32:02 -0300

    docs: Documentação no código

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from utils.valuation import Valuation
-
-# [ ] Salvar dados em um arquivo .txt?
+import numpy as np
+# dividir pelo numero de ações
 
 def main():
 
-    qtd_projecoes = 4
-    n_simulacoes = 10
-    ativo = 'GRND3.SA'
+    anos_projetados = 11 # Quantidade de anos projetados no futuro
+    n_simulacoes = 1000 # iterações no monte carlo
+    ativo = 'GRND3.SA' # Ativo da empresa para pegar alguns dados.
 
-    VALUATION = Valuation(qtd_projecoes, n_simulacoes,ativo)
+    # Classe que faz os cálculos nescessários para o Fluxo de Caixa descontado
+    VALUATION = Valuation(anos_projetados, n_simulacoes,ativo)
     FCD = VALUATION.projetar_fcd()
+
+
+    # print(FCD)
 
     print('concluído !')
 
